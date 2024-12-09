9603e75ea46d4350db01fef72f1c550e1bbfadb7
Federico Griggio <griggio.federico@gmail.com>
2024-12-09 18:50:56 +0100

    changing the header

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-st.markdown("""# This is a header
+st.markdown("""# I'm changing the header
 ## This is a sub header
 
 This is text""")
